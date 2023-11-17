16f396d19740c71b7b2a8a4e4588f3b71111947a
parmeet2311 <73023547+parmeet2311@users.noreply.github.com>
2023-11-17 11:01:26 +0530

    framer

---
+++ src/components/animation/Framer.jsx
@@ -0,0 +1,46 @@
+import { useRef } from "react";
+import { useInView, motion } from "framer-motion";
+
+export function Framer({
+  children,
+  entrance,
+  delay,
+  containerStyle,
+  style,
+  className,
+}) {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  return (
+    <section
+      style={{containerStyle} }
+      className={`overflow-hidden h-full min-h-fit ${className}`}
+      ref={ref}
+    >
+      <motion.div
+        style={{
+          ...style,
+          height: "100%",
+          width: "100%",
+          transform: isInView
+            ? "scale(1)"
+            : entrance === "left"
+            ? "translateX(-200px)"
+            : entrance === "right"
+            ? "translateX(200px)"
+            : entrance === "top"
+            ? "translateY(-200px)"
+            : entrance === "bottom"
+            ? "translateY(200px)"
+            : "",
+          opacity: isInView ? 1 : 0,
+          transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.9s",
+          transitionDelay: isInView ? (delay ? delay : "0.3s") : "0s", // Add a delay of 0.5s when entering the viewport
+        }}
+      >
+        {children}
+      </motion.div>
+    </section>
+  );
+}
